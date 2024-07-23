a6c0f1494e032ad879dd8cf7edfecf4411158697
Jan Zwiener <jan@zwiener.org>
2024-07-23 23:17:58 +0200

    Fix Linux build

--- src/c/benchmark/benchmark.cpp
+++ src/c/benchmark/benchmark.cpp
@@ -72,7 +72,7 @@ static void kalman_test1(void)
     float dz[3];
     // </kalman filter>
 
-    volatile float xr[4] = { 0, 0, 0, 0 }; /* make sure the kalman filter results are not optimized away */
+    volatile float xr[4] = { 0, 0, 0, 0 }; /* volatile: make sure code is not optimized away */
 
     for (int i=0;i<999999;i++)
     {
@@ -92,47 +92,13 @@ static void kalman_test1(void)
         xr[2] = x[2];
         xr[3] = x[3];
     }
+
+    (void)xr;
 }
 
 static void kalman_test2(void)
 {
-    const float sigma = 0.25f;
-    const float bias = 0.66f;
 
-    std::default_random_engine generator;
-    std::normal_distribution<float> distribution(0.0, sigma);
-
-    // <kalman filter>
-    const float R[3 * 3]  = { sigma, 0, 0, 0, sigma, 0, 0, 0, sigma };
-    const float Ht[4 * 3] = { 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1 };
-
-    float x[4]     = { 0, 0, 0, 0 };
-    float P[4 * 4] = { 0.24f, 0, 0, 0, 0, 0.24f, 0, 0, 0, 0, 0.24f, 0, 0, 0, 0, 0.84f };
-
-    float z[3];
-    float dz[3];
-    // </kalman filter>
-
-    volatile float xr[4] = { 0, 0, 0, 0 }; /* make sure the kalman filter results are not optimized away */
-
-    for (int i=0;i<999999;i++)
-    {
-        z[0] = distribution(generator) + bias;
-        z[1] = distribution(generator) + bias;
-        z[2] = distribution(generator) + bias;
-
-        // FIXME calc dz
-        dz[0] = z[0];
-        dz[1] = z[1];
-        dz[2] = z[2];
-
-        nav_kalman(x, P, dz, R, Ht, 4, 3);
-
-        xr[0] = x[0];
-        xr[1] = x[1];
-        xr[2] = x[2];
-        xr[3] = x[3];
-    }
 }
 
 void benchmark(void)
@@ -140,6 +106,7 @@ void benchmark(void)
     auto start = std::chrono::high_resolution_clock::now();
 
     kalman_test1();
+    kalman_test2();
 
     auto end = std::chrono::high_resolution_clock::now();
 
