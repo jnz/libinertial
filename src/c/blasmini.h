289b71cefe343ad599f8cc0427858354e8594a80
Jan Zwiener <jan@zwiener.org>
2024-07-31 22:27:07 +0200

    Refactor: move BLAS functions to blasmini.c module

---
+++ src/c/blasmini.h
@@ -0,0 +1,61 @@
+/** @file blasmini.h
+ * libinertial, Jan Zwiener (jan@zwiener.org)
+ *
+ * @brief Minimal generic BLAS implementation
+ *
+ * Note: all matrices are stored in column-major order.
+ *
+ * @{ */
+
+/******************************************************************************
+ * SYSTEM INCLUDE FILES
+ ******************************************************************************/
+
+/******************************************************************************
+ * PROJECT INCLUDE FILES
+ ******************************************************************************/
+
+/******************************************************************************
+ * DEFINES
+ ******************************************************************************/
+
+/******************************************************************************
+ * TYPEDEFS
+ ******************************************************************************/
+
+/******************************************************************************
+ * LOCAL DATA DEFINITIONS
+ ******************************************************************************/
+
+/******************************************************************************
+ * LOCAL FUNCTION PROTOTYPES
+ ******************************************************************************/
+
+/******************************************************************************
+ * FUNCTION PROTOTYPES
+ ******************************************************************************/
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int lsame(const char* a, const char* b);
+
+int strsm(const char* side, const char* uplo, const char* transa, const char* diag, int* m,
+          int* n, float* alpha, const float* a, int* lda, float* b, int* ldb);
+
+int sgemm(char* transa, char* transb, int* m, int* n, int* k, float* alpha, float* a,
+          int* lda, float* b, int* ldb, float* beta, float* c__, int* ldc);
+
+int ssyrk(char* uplo, char* trans, int* n, int* k, float* alpha, float* a, int* lda,
+          float* beta, float* c__, int* ldc);
+
+int ssymm(char* side, char* uplo, int* m, int* n, float* alpha, float* a, int* lda, float* b,
+          int* ldb, float* beta, float* c__, int* ldc);
+
+#ifdef __cplusplus
+}
+#endif
+
+/* @} */
