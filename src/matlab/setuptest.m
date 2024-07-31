61e798b3f2913eb56374ff6279b7e1e716aaf743
Jan Zwiener <jan@zwiener.org>
2024-07-31 23:35:56 +0200

    MATLAB Test cases updated

--- src/matlab/setuptest.m
+++ src/matlab/setuptest.m
@@ -2,10 +2,10 @@ function [] = setuptest()
 
 rng(42);
 
-epsilon = 1e-4;
+epsilon = 1e-3;
 epsilon_R = 1e-3;
 
-H = [ 1e3 1 1; 1e3 1 1+epsilon];
+H = [ 1e2 1 1; 1e2 1 1+epsilon];
 fprintf('cond(H) = %.5e, rank(H) = %f\n', cond(H), rank(H));
 
 P = eye(3);
@@ -47,10 +47,10 @@ P_carlson = Cxx_carlson*Cxx_carlson';
 
 % Check results
 
-comparevec(x_vanilla, x_takasu, 1e-9, "x_vanilla", "x_takasu");
-comparevec(x_udu,     x_takasu, 1e-9, "x_udu",     "x_takasu");
-comparevec(x_carlson, x_takasu, 1e-9, "x_carlson", "x_takasu");
-comparevec(x_carlson, x_udu,    1e-9, "x_carlson", "x_udu");
+comparevec(x_vanilla, x_takasu, 1e-7, "x_vanilla", "x_takasu");
+comparevec(x_udu,     x_takasu, 1e-7, "x_udu",     "x_takasu");
+comparevec(x_carlson, x_takasu, 1e-7, "x_carlson", "x_takasu");
+comparevec(x_carlson, x_udu,    1e-7, "x_carlson", "x_udu");
 
 checkfilter(x_vanilla, P_vanilla, "Vanilla");
 checkfilter(x_takasu,  P_takasu,  "Takasu");
@@ -89,10 +89,17 @@ dz = z - H*x;
 
 [U, d] = udu(P);
 [x_non_robust,U_1,d_1] = kalman_udu(z,R,H,x,U,d);
+[x_udu_non_robust,U_t,d_t] = kalman_udu_robust(z,R,H,x,U,d,0,false);
+% in this case udu robust must equal the non-robust version:
+assert(abs(x_non_robust-x_udu_non_robust) < 1e-8);
 
 chi2_threshold = 3.8415;
 [x_robust,U_2,d_2] = kalman_udu_robust(z,R,H,x,U,d,chi2_threshold,false);
 
 [x_robust_takasu,P_takasu] = kalman_robust(x,P,dz,R,H,chi2_threshold,false);
+
+assert(abs(x_robust-x_robust_takasu) < 1e-8);
+assert(filterhealthy(x_robust,U_2*diag(d_2)*U_2'));
+assert(filterhealthy(x_robust_takasu,P_takasu));
 
 end
