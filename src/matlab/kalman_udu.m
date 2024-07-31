adc2e9de81bfe113290bcc67446f7d65aa26f5d4
Jan Zwiener <jan@zwiener.org>
2024-07-31 21:26:57 +0200

    MATLAB UDU Filter refactoring (separate file for scalar update)

--- src/matlab/kalman_udu.m
+++ src/matlab/kalman_udu.m
@@ -1,5 +1,5 @@
 function [x,U,d] = kalman_udu(z,R,H,x,U,d)
-% UDU' Bierman Filter Observation Step
+% UDU' Bierman Filter observation step for linear systems
 %
 % Inputs:
 %  z   - measurement vector (m x 1)
@@ -19,55 +19,22 @@ function [x,U,d] = kalman_udu(z,R,H,x,U,d)
 %      navigation, and integration". 1st ed. John Wiley & Sons, New York, 2001.
 
 isdiagonal = isequal(R, diag(diag(R)));
-assert(isdiagonal); % Measurements must not be correlated
-
-% [G] = chol(R); % G'*G = R
-% zdecorr = (G')\z;
-% Hdecorr = (G')\H;
+if (isdiagonal == false)
+    [G] = chol(R); % G'*G = R
+    zdecorr = (G')\z;
+    Hdecorr = (G')\H;
+    Rdecorr = eye(length(z));
+else
+    zdecorr = z;
+    Hdecorr = H;
+    Rdecorr = R;
+end
 
 % Process measurements independently:
 for i=1:size(H,1)
-    [x,U,d] = kalman_udu_scalar(z(i),R(i,i),H(i,:),x,U,d);
+    dz = zdecorr(i)-Hdecorr(i,:)*x;
+    [x,U,d] = kalman_udu_scalar(dz,Rdecorr(i,i),Hdecorr(i,:),x,U,d);
 end
-
-end
-
-function [x,U,d] = kalman_udu_scalar(z,R,H_line,xin,Uin,din)
-% Inputs:
-%  z   - scalar measurement (1 x 1)
-%  R   - variance of measurement error (1 x 1)
-%  H_line - row of measurement sensitivity matrix (m x 1)
-%  x   - a priori estimate of state vector (n x 1)
-%  U   - unit upper triangular factor of covariance matrix of a priori state uncertainty (n x n)
-%  d   - diagonal vector with factor of covariance matrix of a priori state uncertainty (n x 1)
-%
-% Outputs:
-%  x   - a posteriori estimate of state vector (n x 1)
-%  U   - upper unit triangular UD factor of a posteriori state uncertainty covariance (n x n)
-%  d   - diagonal UD factor vector of a posteriori state uncertainty covariance (n x 1)
-
-x     = xin;      % Store inputs into outputs 
-U     = Uin;      % because algorithm does in-place
-d     = din;      % (destructive) calculation of outputs.
-a     = U'*H_line';    % a is not modified, but
-b     = din.*a;   % b is modified to become unscaled Kalman gain.
-dz    = z - H_line*xin;
-alpha = R;
-gamma = 1/alpha;
-for j=1:length(xin)
-    beta   = alpha;
-    alpha  = alpha + a(j)*b(j);
-    lambda = -a(j)*gamma;
-    gamma  = 1/alpha;
-    d(j)   = beta*gamma*d(j);
-    for i=1:j-1
-        beta   = U(i,j);
-        U(i,j) = beta + b(i)*lambda;
-        b(i)   = b(i) + b(j)*beta;
-    end
-end
-dzs = gamma*dz;  % apply scaling to innovations
-x   = x + dzs*b; % multiply by unscaled Kalman gain
 
 end
 
