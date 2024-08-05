5ba769b3ea4bcbec9f36f6434a2a1b4b49138585
Jan Zwiener <jan@zwiener.org>
2024-08-05 22:04:30 +0200

    MATLAB: extended filter health check

--- src/matlab/filterhealthy.m
+++ src/matlab/filterhealthy.m
@@ -1,5 +1,15 @@
 function [healthy] = filterhealthy(x, P, chatty)
-%FILTERHEALTH Check if there are issues in the current filter health.
+%FILTERHEALTHY Check if there are issues in the current filter health.
+% @param[in] x - State vector (n x 1)
+% @param[in] P - Covariance matrix of the state vector (n x n)
+% @param[in] chatty - If set to true, emit debug messages via fprintf.
+% @return True if everything seems to be OK
+
+assert(length(x) == size(P,1));
+assert(size(P,2) == size(P,1));
+if nargin < 3
+    chatty = false;
+end
 
 healthy = true;
 
@@ -17,21 +27,45 @@ if all(isfinite(x)) ~= true
     end
 end
 
-if issymmetric(P) == false
+smallest_entry = min(diag(P));
+if (smallest_entry <= eps)
     healthy = false;
     if chatty
-        fprintf('Matrix P is not symmetric\n');
+        fprintf('Diagonal elements of P close to zero');
     end
 end
 
-try
-    chol(P);
-catch
-    healthy = false;
+lower_tri = tril(P, -1);
+if all(lower_tri(:) == 0)
     if chatty
-        fprintf('Matrix P is not positive definite\n');
+        fprintf('Matrix P lower triangular part is zero\n');
+    end
+    P = P + triu(P,1)';
+    try
+        chol(P);
+    catch
+        healthy = false;
+        if chatty
+            fprintf('Matrix P is not positive definite\n');
+        end
+    end
+else
+    if issymmetric(P) == false
+        healthy = false;
+        if chatty
+            fprintf('Matrix P is not symmetric\n');
+        end
+    end
+    try
+        chol(P);
+    catch
+        healthy = false;
+        if chatty
+            fprintf('Matrix P is not positive definite\n');
+        end
     end
 end
+
 
 end
 
