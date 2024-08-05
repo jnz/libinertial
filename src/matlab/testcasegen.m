f99f259c5a045ed767f51909c9fed4d3bbb87884
Jan Zwiener <jan@zwiener.org>
2024-08-05 22:14:50 +0200

    Minor cleanups on testcase generator

--- src/matlab/testcasegen.m
+++ src/matlab/testcasegen.m
@@ -1,6 +1,8 @@
 function [] = testcasegen()
+% TESTCASEGEN This function helps to generate test cases and numbers for
+% the C/C++ unit tests.
 
-kalman_takasu_test();
+kalman_test();
 kalman_udu_testcase();
 kalman_udu_robust_testcase();
 decorr_testcase();
@@ -8,22 +10,25 @@ kalman_takasu_robust();
 
 end
 
-function [] = kalman_takasu_test()
+function [] = kalman_test()
 
-R = eye(3)*0.25;
-dz = [0.2688; 0.9169; -1.1294];
-H = [8 1 6 1 ; 3 5 7 2; 4 9 2 3 ];
-x = ones(4,1);
-P = eye(4) * 0.04;
+fprintf('<default test>\n');
+R = eye(3)*0.25
+dz = [0.2688; 0.9169; -1.1294]
+H = [8 1 6 1 ; 3 5 7 2; 4 9 2 3 ]
+x = ones(4,1)
+P = eye(4) * 0.04
 [xnew, Pnew, chi2_takasu] = kalman_takasu(x, P, dz, R, H);
 
 xnew
 Pnew
+fprintf('</default test>\n');
 
 end
 
 function [] = kalman_udu_testcase()
 
+fprintf('<udu test>\n');
 R = eye(3)*0.25;
 z = [16.2688, 17.9169, 16.8706]';
 H = [ 8 1 6 1 ; 3 5 7 2; 4 9 2 3];
@@ -44,9 +49,13 @@ xexp
 Uexp'
 dexp
 
+fprintf('</udu test>\n');
+
 end
 
 function [] = kalman_udu_robust_testcase()
+
+fprintf('<robust udu test>\n');
 
 A = [1 1.2; 1.2 1];
 
@@ -71,9 +80,13 @@ x_robust_exp
 U_exp'
 d_exp
 
+fprintf('</robust udu test>\n');
+
 end
 
 function [] = decorr_testcase()
+
+fprintf('<decorrelation test>\n');
 
 x = [15; -2.5; 0];
 H = [1 -0.5 0.25; 0.1 5.0 -2];
@@ -90,9 +103,13 @@ H
 zdecorr = (G')\z
 Hdecorr = (G')\H
 
+fprintf('</decorrelation test>\n');
+
 end
 
 function [] = kalman_takasu_robust()
+
+fprintf('<kalman_takasu outlier test>\n');
 
 R = eye(3) * 0.25;
 dz = [0.2688, 0.9169, -100.1294 ]';
@@ -101,6 +118,8 @@ x = ones(4,1);
 P = eye(4) * 0.04;
 [x, P, chi2_takasu] = kalman_takasu(x, P, dz, R, H);
 chi2_takasu
+
+fprintf('</kalman_takasu outlier test>\n');
 
 end
 
